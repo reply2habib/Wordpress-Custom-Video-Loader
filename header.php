08ef76c1e1eb4bba297e23d5107622c04053b11b
Habib Shaikh <50315847+reply2habib@users.noreply.github.com>
2024-02-13 14:26:17 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,7 @@
-h
+<!-- header.php = Just after Body Tag -->
+<div id="video-container">
+    <video id="fullscreen-video" autoplay muted loop>
+        <source src="https://billboardofcare.com/wp-content/uploads/2024/02/BILLBOARD-OF-CARE-logo-animation.02.mp4">
+        Your browser does not support the video tag.
+    </video>
+</div>
